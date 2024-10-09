09e98590eccafc33567e56450e3302acf8f19788
Jackson Hicken <jacksonhicken@Jacksons-MBP-8.app.byu.edu>
2024-10-09 10:08:07 -0600

    logistic model

--- AmazonEmployeeAccess.R
+++ AmazonEmployeeAccess.R
@@ -21,7 +21,8 @@ library(embed)
 # load data ----------------------------------------------------------------
 
 amazon_train <- vroom("train.csv")
-  
+amazon_train$ACTION <- as.factor(amazon_train$ACTION)
+
 amazon_test <-vroom("test.csv")
 
 my_recipe <- recipe(ACTION ~ ., data = amazon_train) %>% 
@@ -31,5 +32,27 @@ my_recipe <- recipe(ACTION ~ ., data = amazon_train) %>%
 
 prep <- prep(my_recipe)
 bake <- bake(prep, new_data = amazon_train)
+
+
+# logistic regression -----------------------------------------------------
+
+logistic_model <- logistic_reg() %>% 
+  set_engine("glm")
+
+amazon_workflow <- workflow() %>% 
+  add_recipe(my_recipe) %>% 
+  add_model(logistic_model) %>% 
+  fit(data = amazon_train)
+
+view(amazon_predictions) <- predict(amazon_workflow,
+                              new_data = amazon_test,
+                              type = "prob")
+
+kaggle_submission <- amazon_predictions %>%
+  bind_cols(., amazon_test) %>%
+  rename(Action = .pred_1) %>% 
+  select(id, Action)
+
+vroom_write(x=kaggle_submission, file="./amazon_logistic1.csv", delim=",")
 
 
