1845e6289fec89efb40053664f64426b80f88ec1
Jackson Hicken <jacksonhicken@Jacksons-MacBook-Pro-8.local>
2024-12-18 09:52:51 -0700

    Amazon Kaggle competition

--- AmazonEmployeeAccess.R
+++ AmazonEmployeeAccess.R
@@ -1,9 +1,5 @@
 
-##### AMAZON EMPLOYEE ACCESS - STAT 348 #####
-
-
-# packages ----------------------------------------------------------------
-
+library(tidymodels)
 library(tidyverse)
 library(tidymodels)
 library(vroom)
@@ -16,6 +12,7 @@ library(ranger)
 library(stacks)
 library(dbarts)
 library(embed)
+library(kknn)
 
 
 # load data ----------------------------------------------------------------
@@ -30,20 +27,49 @@ my_recipe <- recipe(ACTION ~ ., data = amazon_train) %>%
   step_other(all_nominal_predictors(), threshold = .01) %>% 
   step_dummy(all_nominal_predictors())
 
-prep <- prep(my_recipe)
-bake <- bake(prep, new_data = amazon_train)
+my_recipe <- recipe(ACTION ~ ., data = amazon_train) %>% 
+  step_mutate_at(all_numeric_predictors(), fn = factor) %>% 
+  step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION)) %>% 
+  step_normalize(all_predictors()) %>% 
+  step_pca(all_predictors(), threshold = .9)
+
+my_recipe <- recipe(ACTION~., data = amazon_train) %>% 
+  step_mutate_at(all_numeric_predictors(),fn= factor) %>% 
+  step_other(all_nominal_predictors(), threshold = .01) %>% 
+  #step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION)) %>% 
+  step_normalize(all_predictors()) %>% 
+  step_pca(all_predictors(), threshold = .9)
 
 
 # logistic regression -----------------------------------------------------
 
-logistic_model <- logistic_reg(mixture = .8, penalty = .01) %>% 
+logistic_model <- logistic_reg() %>% 
   set_engine("glm")
 
 amazon_workflow <- workflow() %>% 
   add_recipe(my_recipe) %>% 
-  add_model(logistic_model)
+  add_model()
 
-view(amazon_predictions) <- predict(amazon_workflow,
+tuning_grid <- grid_regular(penalty(),
+                            mixture(),
+                            levels = 5)
+
+folds <- vfold_cv(amazon_train, v =5, repeats = 1)
+
+CV_results <- amazon_workflow %>% 
+  tune_grid(resamples = folds,
+            grid = tuning_grid, 
+            metrics = metric_set(roc_auc))
+
+bestTune <- CV_results %>% 
+  select_best("roc_auc")
+
+final_wf <- amazon_workflow %>% 
+  finalize_workflow(bestTune) %>% 
+  fit(data = amazon_train)
+
+
+amazon_predictions <- predict(amazon_workflow,
                               new_data = amazon_test,
                               type = "prob")
 
@@ -52,9 +78,10 @@ kaggle_submission <- amazon_predictions %>%
   rename(Action = .pred_1) %>% 
   select(id, Action)
 
-vroom_write(x=kaggle_submission, file="./amazon_logistic2.csv", delim=",")
+vroom_write(x=kaggle_submission, file="./amazon_logistic1_pca.csv", delim=",")
 
-# penalized ######################
+
+# penalized regression ----------------------------------------------------
 
 my_recipe1 <- recipe(ACTION ~ ., data = amazon_train) %>% 
   step_mutate_at(all_numeric_predictors(), fn = factor) %>% 
@@ -83,4 +110,128 @@ final_wf <- amazon_workflow %>%
 amazon_predictions <- predict(final_wf, new_data = amazon_test,
                        type = "prob")
 
+kaggle_submission <- amazon_predictions %>%
+  bind_cols(., amazon_test) %>%
+  rename(Action = .pred_1) %>% 
+  select(id, Action)
+
+vroom_write(x=kaggle_submission, file="./amazon_logistic1_pca.csv", delim=",")
+
+
+# knn ---------------------------------------------------------------------
+
+n <- count(amazon_test)
+
+knn_model <- nearest_neighbor(neighbors = sqrt(n)) %>% 
+  set_mode("classification") %>% 
+  set_engine("kknn")
+
+my_recipe <- recipe(ACTION ~ ., data = amazon_train) %>% 
+  step_mutate_at(all_numeric_predictors(), fn = factor) %>% 
+  step_other(all_nominal_predictors(), threshold = .01) %>% 
+  step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION))
+
+my_recipe <- recipe(ACTION ~ ., data = amazon_train) %>% 
+  step_mutate_at(all_numeric_predictors(), fn = factor) %>% 
+  step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION)) %>% 
+  step_normalize(all_predictors()) %>% 
+  step_pca(all_predictors(), threshold = .9)
+
+knn_wf <- workflow() %>% 
+  add_recipe(my_recipe) %>% 
+  add_model(knn_model) %>% 
+  fit(data = amazon_train)
+
+kknn_preds <- predict(knn_wf, new_data = amazon_test, type = "prob")
+
+kaggle_submission <- kknn_preds %>%
+  bind_cols(., amazon_test) %>%
+  rename(Action = .pred_1) %>% 
+  select(id, Action)
+
+vroom_write(x=kaggle_submission, file="./amazon_kknn_pca.csv", delim=",")
+
+
+# random forest -----------------------------------------------------------
+
+rf_model <- rand_forest(mtry = tune(),
+                        min_n = tune(),
+                        trees = 1000) %>% 
+  set_engine("ranger") %>% 
+  set_mode("classification")
+
+rf_wf <- workflow() %>% 
+  add_recipe(my_recipe) %>% 
+  add_model(rf_model)
+
+
+tuning_grid <- grid_regular(mtry(range = c(1,9)),
+                            min_n(),
+                            levels = 5)
+
+folds <- vfold_cv(amazon_train, v = 10, repeats = 1)
+
+CV_results <- rf_wf %>%
+  tune_grid(resamples=folds,
+            grid=tuning_grid,
+            metrics=metric_set(roc_auc))
+
+bestTune <- CV_results %>%
+  select_best()
+
+final_wf <- rf_wf %>% 
+  finalize_workflow(bestTune) %>% 
+  fit(data = amazon_train)
+
+rf_preds <- predict(final_wf,
+                    new_data = amazon_test,
+                    type = "prob")
+
+kaggle_submission <- rf_preds %>%
+  bind_cols(., amazon_test) %>%
+  rename(Action = .pred_1) %>% 
+  select(id, Action)
+
+vroom_write(x=kaggle_submission, file="./amazon_rf_final2.csv", delim=",")
+
+
+# naive bayes -------------------------------------------------------------
+
+nb_model <- naive_Bayes(Laplace = tune(), smoothness = tune()) %>% 
+  set_mode("classification") %>% 
+  set_engine("naivebayes")
+
+nb_wf <- workflow() %>% 
+  add_recipe(my_recipe) %>% 
+  add_model(nb_model)
+
+tuning_grid <- grid_regular(Laplace(),
+                            smoothness(),
+                            levels = 5)
+
+folds <- vfold_cv(amazon_train, v = 5, repeats=1)
+
+
+CV_results <- nb_wf %>%
+  tune_grid(resamples=folds,
+            grid=tuning_grid,
+            metrics=metric_set(roc_auc))
+
+bestTune <- CV_results %>%
+  select_best()
+
+final_wf <- nb_wf %>% 
+  finalize_workflow(bestTune) %>% 
+  fit(data = amazon_train)
+
+
+amazon_predictions <- predict(final_wf, new_data = amazon_test,
+                              type = "prob")
+
+kaggle_submission <- amazon_predictions %>%
+  bind_cols(., amazon_test) %>%
+  rename(Action = .pred_1) %>% 
+  select(id, Action)
+
+vroom_write(x=kaggle_submission, file="./amazon_nb_pca_smote2.csv", delim=",")
 
